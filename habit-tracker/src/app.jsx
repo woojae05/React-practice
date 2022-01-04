49e29ad6fd19f2784316747ff7a3114d19a3fc90
woojae <dnwo0526@dgsw.hs.kr>
2022-01-05 01:08:15 +0900

    ⚡ ::count기능추가

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -1,8 +1,11 @@
 import './app.css';
+import Habit from './components/habit';
 
 function App() {
   return (
-    <h1>hello</h1>
+    <>
+      <Habit/>
+    </>
   );
 }
 
--- habit-tracker/src/components/habit.jsx
+++ habit-tracker/src/components/habit.jsx
@@ -1,11 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+class Habit extends Component {
 
-export default class habit extends Component {
+    state = {
+        count: 0,
+    }
+
+    handleIncrement = () =>{
+        this.setState({count: this.state.count+1})
+    }
+    handleDecrement  = () =>{
+        this.state.count>0 && this.setState({count: this.state.count-1})
+    }
     render() {
         return (
-            <div>
-                
-            </div>
-        )
+            <li className="habit">
+                <span className="habit-name">Readign</span>
+                <span className="habit-count">{this.state.count}</span>
+                <button className='habit-button habit-increase' onClick={this.handleIncrement}> 
+                    <i className="fas fa-plus-square"></i>
+                </button>
+                <button className='habit-button habit-decrease' onClick={this.handleDecrement}> 
+                    <i className="fas fa-minus-square"></i>
+                </button>
+                <button className='habit-button habit-delete'>
+                    <i className="fas fa-trash"></i>
+                </button>
+
+
+            </li>
+        );
     }
 }
+
+export default Habit;
