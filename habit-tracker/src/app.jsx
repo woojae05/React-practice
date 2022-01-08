7b28372d815f4a5fa47c81ad67b8cbe3aff02906
정우재 <80890472+woojae05@users.noreply.github.com>
2022-01-09 01:27:54 +0900

    ⚡:: totalCount 기능추가

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -1,12 +1,61 @@
-import './app.css';
+import React, { Component } from 'react'
 import Habits from './components/habits';
+import './app.css'
+import Navbar from './components/navbar';
+class App extends Component {
+  state = {
+    habits:[
+        { id: 1, name: 'Reading', count: 0},
+        { id: 2, name: 'coding', count: 0},
+        { id: 3, name: 'Riding' ,count: 0}, 
+        { id: 4, name: 'Riding' ,count: 0}, 
 
-function App() {
-  return (
-    <>
-      <Habits/>
-    </>
-  );
+    ],
+    total:{
+      count: 0
+    }
+  }
+  
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits]; //새로운 배열 생성 why? 직접적으로 오브젝트를 수정하는 것은 좋지 않은 방법
+    const index  = this.state.habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({habits}) // key 와 value가 같다면 생략 가능
+    const total  = this.state.total
+    this.state.habits.map(habit=>{
+     total.count+= habit.count
+   })
+ }
+
+ handleDecrement  = (habit) =>{
+     const habits = [...this.state.habits];
+     const index  = this.state.habits.indexOf(habit);
+     habits[index].count && habits[index].count--;
+     this.setState({habits})
+     
+ }
+
+ handleDelete = (habit) =>{
+     const habits = this.state.habits.filter(item => item.id !== habit.id) //fiter를 사용해 배열을 생성하는 방법
+     // const habits = [...this.state.habits];  
+     // const index  = this.state.habits.indexOf(habit); 
+     // habits.splice(index,1); // splice를 사용해 특정 인덱스를 삭제하는 방법
+     this.setState({habits}) 
+ }
+ 
+  render() {
+    return (
+      <>
+        <Navbar totalCount={this.state.habits.filter(item => item.count > 0).length}/>
+        <Habits
+         habits={this.state.habits}        
+         onIncrement={this.handleIncrement}
+         onDecrement={this.handleDecrement}
+         onDelete={this.handleDelete}>
+         </Habits>
+      </>
+    )
+  }
 }
 
 export default App;
--- habit-tracker/src/components/habits.jsx
+++ habit-tracker/src/components/habits.jsx
@@ -1,48 +1,18 @@
 import React, { Component } from 'react'
 import Habit from './habit'
 class Habits extends Component {
-    state = {
-        habits:[
-            { id: 1, name: 'Reading', count: 0},
-            { id: 2, name: 'coding', count: 0},
-            { id: 3, name: 'Riding' ,count: 0}, 
-        ],
-    }
-
-    handleIncrement = (habit) =>{
-       const habits = [...this.state.habits]; // 배열 복사, 새로운 배열 생성 why? 직접적으로 배열을 수정하는 것은 좋지 않은 방법
-       const index  = this.state.habits.indexOf(habit);
-       habits[index].count++;
-       this.setState({habits}) // key 와 value가 같다면 생략 가능
-    }
-
-    handleDecrement  = (habit) =>{
-        const habits = [...this.state.habits];
-        const index  = this.state.habits.indexOf(habit);
-        habits[index].count && habits[index].count--;
-        this.setState({habits})
-    }
-
-    handleDelete = (habit) =>{
-        console.log("hi")
-        const habits = this.state.habits.filter(item => item.id !== habit.id) //fiter를 사용해 배열을 생성하는 방법
-        // const habits = [...this.state.habits];  
-        // const index  = this.state.habits.indexOf(habit); 
-        // habits.splice(index,1); // splice를 사용해 특정 인덱스를 삭제하는 방법
-        this.setState({habits}) 
-    }
 
     render() {
         return (
             <ul >
                 {
-                    this.state.habits.map(habit=>((
+                    this.props.habits.map(habit=>((
                         <Habit
                          key={habit.id}
                          habit={habit}
-                         onIncrement={this.handleIncrement}
-                         onDecrement={this.handleDecrement}
-                         onDelete={this.handleDelete}/>
+                         onIncrement={this.props.onIncrement}
+                         onDecrement={this.props.onDecrement}
+                         onDelete={this.props.onDelete}/>
                      )))
                 }
             </ul>
