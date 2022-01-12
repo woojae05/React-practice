93e65c95aad25173a2ea107c993e4f5b629796e3
정우재 <80890472+woojae05@users.noreply.github.com>
2022-01-13 01:57:53 +0900

    create: simpleHabit with hooks

--- habit-tracker/src/index.js
+++ habit-tracker/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
 import '@fortawesome/fontawesome-free/js/all'
+import SimpleHabit from './components/simpleHabit';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <SimpleHabit/>
   </React.StrictMode>,
   document.getElementById('root')
 );
